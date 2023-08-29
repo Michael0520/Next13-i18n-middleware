e648a0a72f9a9c111c9b270a40d4f9e6c80c9414
Michael0520 <po2319987@gmail.com>
2023-08-30 02:19:50 +0800

    feat: 🎸 add i18n dictionaryEntry

--- content.ts
+++ content.ts
@@ -1,27 +1,20 @@
 interface DictionaryEntry {
-    homePage_Header: string
-    homePage_Content: string
-    aboutPage_Header: string
-    aboutPage_Content: string
+    homeHeader: string
+    homeContent: string
+
 }
 
 export const dictionary: Record<string, DictionaryEntry> = {
     en: {
-        homePage_Header: "Home",
-        homePage_Content: "Welcome to my home.",
-        aboutPage_Header: "About Me",
-        aboutPage_Content: "Here is some information about me. English is my primary language."
+        homeHeader: "Home",
+        homeContent: "Welcome to my home.",
     },
     es: {
-        homePage_Header: "Casa",
-        homePage_Content: "Beinvenidos a mi casa.",
-        aboutPage_Header: "Sobre Yo",
-        aboutPage_Content: "Aqui tenemos un poco informacion sobre yo. Hablo espanol tambien."
+        homeHeader: "Casa",
+        homeContent: "Beinvenidos a mi casa.",
     },
     id: {
-        homePage_Header: "Rumah",
-        homePage_Content: "Selamat datang di rumah saya.",
-        aboutPage_Header: "Tentang Saya",
-        aboutPage_Content: "Berikut adalah beberapa informasi tentang saya. Bahasa Inggris adalah bahasa utama saya."
+        homeHeader: "Rumah",
+        homeContent: "Selamat datang di rumah saya.",
     }
 }
