d0c5698b0a0578f47428c0976f6cdd160ca71339
Michael0520 <po2319987@gmail.com>
2023-08-30 02:22:19 +0800

    feat: 🎸 update i18n dictionaryEntry

--- content.ts
+++ content.ts
@@ -1,20 +1,27 @@
 interface DictionaryEntry {
     homeHeader: string
     homeContent: string
-
+    aboutHeader: string
+    aboutContent: string
 }
 
 export const dictionary: Record<string, DictionaryEntry> = {
     en: {
         homeHeader: "Home",
         homeContent: "Welcome to my home.",
+        aboutHeader: "About",
+        aboutContent: "This is the about page.",
     },
     es: {
         homeHeader: "Casa",
         homeContent: "Beinvenidos a mi casa.",
+        aboutHeader: "Acerca de",
+        aboutContent: "Esta es la página sobre nosotros.",
     },
     id: {
         homeHeader: "Rumah",
         homeContent: "Selamat datang di rumah saya.",
+        aboutHeader: "Tentang",
+        aboutContent: "Ini adalah halaman tentang kami.",
     }
 }
